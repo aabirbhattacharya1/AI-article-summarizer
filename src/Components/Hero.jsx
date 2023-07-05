9026f4824340951dfd9c6e317dde012df9c1659c
aabirbhattacharya1 <118759440+aabirbhattacharya1@users.noreply.github.com>
2023-07-06 04:23:05 +0530

    sc

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/aabirbhattacharya1/AI-article-summarizer" , "_blank")
           }
           className='black_btn'
         >
